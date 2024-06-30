51986d1c490c146691df1ff4963f4195ebeff044
Artem Mukhin <artem.m.mukhin@gmail.com>
2024-06-30 12:21:46 +0200

    Add validator to `todoSchema`

--- server/models/todo.js
+++ server/models/todo.js
@@ -2,7 +2,23 @@ const mongoose = require('mongoose');
 
 const todoSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  state: { type: String, enum: ['TODO', 'ONGOING', 'DONE'], default: 'TODO' },
+  state: {
+    type: String,
+    enum: ['TODO', 'ONGOING', 'DONE'],
+    default: 'TODO',
+    validate: {
+      validator: function(v) {
+        if (this.isNew) return true; // Allow any initial state
+        const validTransitions = {
+          'TODO': ['ONGOING'],
+          'ONGOING': ['DONE', 'TODO'],
+          'DONE': ['ONGOING']
+        };
+        return validTransitions[this.state] && validTransitions[this.state].includes(v);
+      },
+      message: props => `${props.value} is not a valid state transition from ${this.state}`
+    }
+  },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   todoList: { type: mongoose.Schema.Types.ObjectId, ref: 'TodoList', required: true },
   updatedAt: { type: Date, default: Date.now }
